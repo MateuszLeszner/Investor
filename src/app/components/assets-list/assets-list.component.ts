6419e1b5618594b78900207cb943ba69b7ba641f
Mateusz Leszner <mateusz@leszner.eu>
2022-09-04 00:34:38 +0200

    INW-11 - assets summary

--- src/app/components/assets-list/assets-list.component.ts
+++ src/app/components/assets-list/assets-list.component.ts
@@ -19,4 +19,22 @@ export class AssetsListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  public getTotalMoneySpent(): number | undefined {
+    return this.assets?.map(asset => asset.moneySpent).reduce((a, b) => Number(a) + Number(b), 0);
+  }
+
+  public getTotalCurrentValue(): number | undefined {
+    return this.assets?.map(asset => Asset.getCurrentTotalValue(asset)).reduce((a, b) => Number(a) + Number(b), 0);
+  }
+
+  public getTotalProfit(): number | undefined {
+    return this.assets?.map(asset => Asset.getProfit(asset)).reduce((a, b) => Number(a) + Number(b), 0);
+  }
+
+  public getTotalPercentageProfit(): number | undefined {
+    const totalProfit = this.getTotalProfit();
+    const totalMoneySpent = this.getTotalMoneySpent();
+    return totalProfit && totalMoneySpent ? totalProfit / totalMoneySpent : undefined;
+  }
 }
